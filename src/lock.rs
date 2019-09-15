853c291d105deb28db4a7d1f21479b11a96f42d1
Marco Cameriero <me@marcocameriero.net>
2019-09-15 15:55:37 +0200

    Move more code away from main entrypoint.

--- src/lock.rs
+++ src/lock.rs
@@ -1,12 +1,18 @@
 use std::cell::RefCell;
+use std::cmp::min;
 use std::fs::{File, OpenOptions};
-use std::io::{Read, Write};
+use std::io::{self, Read, Write};
 use std::path::Path;
 use std::rc::Rc;
+use std::time::Duration;
 use atoi::atoi;
-use vt::{Console, Vt, VtNumber};
+use termion::event::Key;
+use termion::input::TermRead;
+use vt::{Console, Vt, VtNumber, VtFlushType};
+use crate::auth;
 use crate::options::Opt;
 use crate::error::*;
+use crate::util;
 
 const SYSRQ_PATH: &str = "/proc/sys/kernel/sysrq";
 const PRINTK_PATH: &str = "/proc/sys/kernel/printk";
@@ -33,6 +39,86 @@ fn write_u32_to_file<P>(path: P, data: u32) -> Result<()>
     Ok(())
 }
 
+fn user_selection<'a, W, R>(users: &'a [String], current_user: &'a str, vt: &mut W, input: &mut R) -> Result<&'a String>
+    where W: Write,
+          R: Iterator<Item = io::Result<termion::event::Key>>
+{
+    let mut current_index = users.iter().position(|x| x == current_user).unwrap();
+    'outer: loop {
+
+        // Clear the terminal
+        write!(vt, "{}", termion::clear::All).context(ErrorKind::Io)?;
+
+        // A nice message with the list of users allowed to unlock
+        write!(vt,
+            "{}The following users are unthorized to unlock:{}",
+            termion::cursor::Goto(1, 2),
+            termion::cursor::Goto(1, 4)).context(ErrorKind::Io)?;
+        for (i, u) in users.iter().enumerate() {
+            if current_index == i {
+                write!(vt,
+                       "{}{}=> {}{}\n\r",
+                       termion::style::Bold,
+                       termion::color::Fg(termion::color::LightBlue),
+                       u,
+                       termion::style::Reset).context(ErrorKind::Io)?;
+            } else {
+                write!(vt, " - {}\n\r", u).context(ErrorKind::Io)?;
+            }
+        }
+
+        write!(vt, "\nUse the arrow keys to select the user that wants to unlock and press enter.").context(ErrorKind::Io)?;
+
+        while let Some(c) = input.next() {
+            match c.context(ErrorKind::Io)? {
+                Key::Up => {
+                    current_index = current_index.saturating_sub(1);
+                    continue 'outer;
+                },
+                Key::Down => {
+                    current_index = min(current_index + 1, users.len() - 1);
+                    continue 'outer;
+                },
+                Key::Char('\n') => {
+                    break 'outer;
+                }
+                _ => ()
+            }
+        }
+
+        return Err(ErrorKind::Message("Unexpected EOF.").into());
+
+    }
+
+    Ok(&users[current_index])
+}
+
+fn repaint_console<W: Write>(opt: &Opt, vt: &mut W, user: &str) -> Result<()> {
+    
+    // Clear the terminal
+    write!(vt, "{}", termion::clear::All).context(ErrorKind::Io)?;
+
+    // Print the unlock message
+    let mut line = 2;
+    if let Some(ref message) = opt.message {
+        writeln!(vt,
+                 "{}{}",
+                 termion::cursor::Goto(1, line),
+                 message).context(ErrorKind::Io)?;
+        line += 2;
+    }
+    write!(vt,
+           "{}Press enter to unlock as {}{}{}{}. [Press Ctrl+C to change user] ",
+           termion::cursor::Goto(1, line),
+           termion::style::Bold,
+           termion::color::Fg(termion::color::LightBlue),
+           user,
+           termion::style::Reset
+           ).context(ErrorKind::Io)?;
+
+    Ok(())
+}
+
 /// Whole-station lock.
 /// 
 /// For as long as a `Lock` structure is in scope, the station is locked.
@@ -40,13 +126,12 @@ fn write_u32_to_file<P>(path: P, data: u32) -> Result<()>
 /// 
 /// Lock the station by calling [`Lock::with_options`](crate::lock::Lock::with_options).
 pub struct Lock<'a> {
+    opt: Opt,
     console: &'a Console,
     original_vt: VtNumber,
     original_sysrq: Option<u32>,
     original_printk: Option<u32>,
-    lock_vt: Rc<RefCell<Vt<'a>>>,
-    vt_switch_locked: bool,
-    vt_blanked: bool
+    lock_vt: Rc<RefCell<Vt<'a>>>
 }
 
 impl<'a> Lock<'a> {
@@ -55,7 +140,9 @@ impl<'a> Lock<'a> {
     /// 
     /// This function switches to a new virtual terminal and inhibits sysrequests
     /// and kernel messages (unless disabled by the options).
-    pub fn with_options(opt: &Opt, console: &'a Console, lock_vt: Rc<RefCell<Vt<'a>>>) -> Result<Lock<'a>> {
+    pub fn new(opt: Opt, console: &'a Console, lock_vt: Vt<'a>) -> Result<Lock<'a>> {
+
+        let lock_vt = Rc::new(RefCell::new(lock_vt));
 
         // Save the current vt
         let original_vt = console.current_vt_number()
@@ -103,17 +190,94 @@ impl<'a> Lock<'a> {
         }
 
         Ok(Lock {
+            opt,
             console,
             original_vt,
             original_sysrq,
             original_printk,
-            lock_vt,
-            vt_switch_locked: !opt.no_lock,
-            vt_blanked: opt.dark
+            lock_vt
         })
 
     }
-    
+
+    /// Runs the main authentication loop. Returns when the user has successfully unlocked the station.
+    pub fn run_loop(&mut self) -> Result<()> {
+        
+        // Put the terminal in raw mode
+        self.lock_vt.borrow_mut().raw().context(ErrorKind::Io)?;
+        let (reader, mut writer) = util::split_stream(Rc::clone(&self.lock_vt));
+        let mut reader = reader.keys();
+
+        let mut user = self.opt.users.first().unwrap();
+        'outer: loop {
+
+            // Flush the terminal buffers
+            self.lock_vt.borrow_mut().flush_buffers(VtFlushType::Both).context(ErrorKind::Io)?;
+
+            // Repaint the console
+            repaint_console(&self.opt, &mut writer, user)?;
+
+            // Wait for enter to be pressed if not in quick mode.
+            // If we are in quick mode, instead, jump directly to
+            // authentication, and disable quick mode, so that after
+            // a failed attempt, it will be requested to press enter.
+            //
+            // This way, if both quick mode and dark mode are enabled,
+            // the user will be able to make a first login attempt
+            // with the screen switched off, and then it will be turned on later.
+            if !self.opt.quick {
+
+                // Wait for enter
+                for c in &mut reader {
+                    match c.context(ErrorKind::Io)? {
+                        Key::Char('\n') => {
+                            break;
+                        },
+                        Key::Ctrl('c') => {
+                            
+                            // Switch the screen back on before user selection
+                            if self.opt.dark {
+                                self.lock_vt.borrow_mut().blank(false).context(ErrorKind::Io)?;
+                            }
+                            user = user_selection(&self.opt.users, user, &mut writer, &mut reader)?;
+                            continue 'outer;
+
+                        },
+                        _ => {}
+                    }
+                }
+
+                // Switch the screen back on during authentication
+                if self.opt.dark {
+                    self.lock_vt.borrow_mut().blank(false).context(ErrorKind::Io)?;
+                }
+
+                // Repaint the console
+                repaint_console(&self.opt, &mut writer, user)?;
+
+            } else {
+                self.opt.quick = false;
+            }
+
+            write!(writer, "\n\r").context(ErrorKind::Io)?;
+
+            // Try to authenticate the user
+            if auth::authenticate_user(user, auth::VtConverse::new(Rc::clone(&self.lock_vt)))? {
+                break;
+            }
+
+            // Switch the screen back on to be sure that the user knows
+            // the authentication failed.
+            if self.opt.dark {
+                self.lock_vt.borrow_mut().blank(false).context(ErrorKind::Io)?;
+            }
+
+            write!(writer, "\nAuthentication failed.\n\r").context(ErrorKind::Io)?;
+            ::std::thread::sleep(Duration::from_secs(3));
+        }
+
+        Ok(())
+    }
 }
 
 impl<'a> Drop for Lock<'a> {
@@ -122,12 +286,12 @@ impl<'a> Drop for Lock<'a> {
     fn drop(&mut self) {
 
         // Unblank the terminal
-        if self.vt_blanked {
+        if self.opt.dark {
             let _ = self.lock_vt.borrow_mut().blank(false);
         }
 
         // Re-enable vt switching
-        if self.vt_switch_locked {
+        if !self.opt.no_lock {
             let _ = self.console.lock_switch(false);
         }
 
--- src/main.rs
+++ src/main.rs
@@ -4,21 +4,13 @@ mod lock;
 mod auth;
 mod util;
 
-use std::cell::RefCell;
-use std::cmp::min;
 use std::fs::File;
-use std::io::{self, Write, Read};
+use std::io::{Write, Read};
 use std::os::unix::io::FromRawFd;
-use std::time::Duration;
-use std::rc::Rc;
 use failure::Fail;
 use nix::sys::wait::{WaitStatus, waitpid};
 use nix::unistd::{Uid, Gid, ForkResult, geteuid, setresuid, setresgid, setsid, fork, close};
-use vt::VtFlushType;
-use termion::event::Key;
-use termion::input::TermRead;
 use crate::error::*;
-use crate::options::Opt;
 
 /// This is because a lower-numbered vt might be actually free, but systemd-logind is managing it,
 /// and we don't want to step on systemd, otherwise bad things will happen.
@@ -26,90 +18,10 @@ use crate::options::Opt;
 /// On most systems, the maximum number of vts is 16 or 64, so this should not be a problem.
 const MIN_VT_NUMBER: i32 = 13;
 
-fn user_selection<'a, W, R>(users: &'a [String], current_user: &'a str, vt: &mut W, input: &mut R) -> Result<&'a String>
-    where W: Write,
-          R: Iterator<Item = io::Result<termion::event::Key>>
-{
-    let mut current_index = users.iter().position(|x| x == current_user).unwrap();
-    'outer: loop {
-
-        // Clear the terminal
-        write!(vt, "{}", termion::clear::All).context(ErrorKind::Io)?;
-
-        // A nice message with the list of users allowed to unlock
-        write!(vt,
-            "{}The following users are unthorized to unlock:{}",
-            termion::cursor::Goto(1, 2),
-            termion::cursor::Goto(1, 4)).context(ErrorKind::Io)?;
-        for (i, u) in users.iter().enumerate() {
-            if current_index == i {
-                write!(vt,
-                       "{}{}=> {}{}\n\r",
-                       termion::style::Bold,
-                       termion::color::Fg(termion::color::LightBlue),
-                       u,
-                       termion::style::Reset).context(ErrorKind::Io)?;
-            } else {
-                write!(vt, " - {}\n\r", u).context(ErrorKind::Io)?;
-            }
-        }
-
-        write!(vt, "\nUse the arrow keys to select the user that wants to unlock and press enter.").context(ErrorKind::Io)?;
-
-        while let Some(c) = input.next() {
-            match c.context(ErrorKind::Io)? {
-                Key::Up => {
-                    current_index = current_index.saturating_sub(1);
-                    continue 'outer;
-                },
-                Key::Down => {
-                    current_index = min(current_index + 1, users.len() - 1);
-                    continue 'outer;
-                },
-                Key::Char('\n') => {
-                    break 'outer;
-                }
-                _ => ()
-            }
-        }
-
-        return Err(ErrorKind::Message("Unexpected EOF.").into());
-
-    }
-
-    Ok(&users[current_index])
-}
-
-fn repaint_console<W: Write>(opt: &Opt, vt: &mut W, user: &str) -> Result<()> {
-    
-    // Clear the terminal
-    write!(vt, "{}", termion::clear::All).context(ErrorKind::Io)?;
-
-    // Print the unlock message
-    let mut line = 2;
-    if let Some(ref message) = opt.message {
-        writeln!(vt,
-                 "{}{}",
-                 termion::cursor::Goto(1, line),
-                 message).context(ErrorKind::Io)?;
-        line += 2;
-    }
-    write!(vt,
-           "{}Press enter to unlock as {}{}{}{}. [Press Ctrl+C to change user] ",
-           termion::cursor::Goto(1, line),
-           termion::style::Bold,
-           termion::color::Fg(termion::color::LightBlue),
-           user,
-           termion::style::Reset
-           ).context(ErrorKind::Io)?;
-
-    Ok(())
-}
-
 fn run() -> Result<i32> {
 
     // Parse the options
-    let mut opt = options::parse();
+    let opt = options::parse();
 
     // We need to run as root or setuid root
     if !geteuid().is_root() {
@@ -173,88 +85,17 @@ fn run() -> Result<i32> {
     let console = vt::Console::open().context(ErrorKind::Message("Cannot open console device file"))?;
     
     // Lock station
-    let vt = Rc::new(RefCell::new(
-        console.new_vt_with_minimum_number(MIN_VT_NUMBER).context(ErrorKind::Message("Cannot allocate new terminal"))?
-    ));
-    let _lock = lock::Lock::with_options(&opt, &console, Rc::clone(&vt))?;
-
-    // Put the terminal in raw mode
-    vt.borrow_mut().raw().context(ErrorKind::Io)?;
-    let (reader, mut writer) = util::split_stream(Rc::clone(&vt));
-    let mut reader = reader.keys();
+    let vt = console.new_vt_with_minimum_number(MIN_VT_NUMBER).context(ErrorKind::Message("Cannot allocate new terminal"))?;
+    let mut lock = lock::Lock::new(opt, &console, vt)?;
 
     // Signal to the parent process that the station has been fully locked
-    let mut pipe_w = unsafe { File::from_raw_fd(pipe.1) };
-    pipe_w.write_all(&[ 0x42u8 ]).context(ErrorKind::Io)?;
+    {
+        let mut pipe_w = unsafe { File::from_raw_fd(pipe.1) };
+        pipe_w.write_all(&[ 0x42u8 ]).context(ErrorKind::Io)?;
+    }
 
     // The auth loop
-    let mut user = opt.users.first().unwrap();
-    'outer: loop {
-
-        // Flush the terminal buffers
-        vt.borrow_mut().flush_buffers(VtFlushType::Both).context(ErrorKind::Io)?;
-
-        // Repaint the console
-        repaint_console(&opt, &mut writer, user)?;
-
-        // Wait for enter to be pressed if not in quick mode.
-        // If we are in quick mode, instead, jump directly to
-        // authentication, and disable quick mode, so that after
-        // a failed attempt, it will be requested to press enter.
-        //
-        // This way, if both quick mode and dark mode are enabled,
-        // the user will be able to make a first login attempt
-        // with the screen switched off, and then it will be turned on later.
-        if !opt.quick {
-
-            // Wait for enter
-            for c in &mut reader {
-                match c.context(ErrorKind::Io)? {
-                    Key::Char('\n') => {
-                        break;
-                    },
-                    Key::Ctrl('c') => {
-                        
-                        // Switch the screen back on before user selection
-                        if opt.dark {
-                            vt.borrow_mut().blank(false).context(ErrorKind::Io)?;
-                        }
-                        user = user_selection(&opt.users, user, &mut writer, &mut reader)?;
-                        continue 'outer;
-
-                    },
-                    _ => {}
-                }
-            }
-
-            // Switch the screen back on during authentication
-            if opt.dark {
-                vt.borrow_mut().blank(false).context(ErrorKind::Io)?;
-            }
-
-            // Repaint the console
-            repaint_console(&opt, &mut writer, user)?;
-
-        } else {
-            opt.quick = false;
-        }
-
-        write!(writer, "\n\r").context(ErrorKind::Io)?;
-
-        // Try to authenticate the user
-        if auth::authenticate_user(user, auth::VtConverse::new(Rc::clone(&vt)))? {
-            break;
-        }
-
-        // Switch the screen back on to be sure that the user knows
-        // the authentication failed.
-        if opt.dark {
-            vt.borrow_mut().blank(false).context(ErrorKind::Io)?;
-        }
-
-        write!(writer, "\nAuthentication failed.\n\r").context(ErrorKind::Io)?;
-        ::std::thread::sleep(Duration::from_secs(3));
-    }
+    lock.run_loop()?;
 
     Ok(0)
 }
